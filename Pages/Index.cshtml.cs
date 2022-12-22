21da19adbd30b56121f5557b9a50fc447feeacdd
Nino van der Mark <ninovdmark@gmail.com>
2022-12-22 11:49:13 +0100

    Example of Razor runtime compilation failing onC# 11 features

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorRequiredField.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public bool Method(SampleModel model)
+    {
+        if (model.Name.Contains("false"))
+            return false;
+        
+        return true;
+    }
+}
---
+++ SampleModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RazorRequiredField;
+
+public class SampleModel
+{
+    public required string Name { get; init; }
+}
